95c8158f213cd79b8a5e80403120e15945bf62ee
Radu Suciu <radusuciu@gmail.com>
2016-04-21 00:50:20 -0700

    Ensure celery can install after python deps are loaded

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-cd /home/cravattdb/cravattdb/cravattdb
-npm install
-bower install -F
-
-# start celery daemon
-celery -A cravattdb.models.tasks worker --loglevel=info --detach
-
-# install requirements in separate virtual environment
+# install requirements in virtual environment
 cd /home/cravattdb/cravattdb
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
+
+# start celery daemon
+celery -A cravattdb.models.tasks worker --loglevel=info --detach
+
+# get client software
+cd /home/cravattdb/cravattdb/cravattdb
+npm install
+bower install -F
 
 # run the server
 python run.py
