63c20064e90949a4d09e0a7463f850c9b2bfda14
Radu Suciu <radusuciu@gmail.com>
2016-05-11 12:21:23 -0700

    Re-implement dataset display

--- cravattdb/__init__.py
+++ cravattdb/__init__.py
@@ -111,7 +111,13 @@ def render_experiments():
 @app.route('/dataset/<int:experiment_id>')
 @login_required
 def render_dataset(experiment_id):
-    return render_template('index.html', bootstrap=api.get_dataset(experiment_id))
+    raw = api.get_dataset(experiment_id)
+    raw = raw['dataset']
+
+    return render_template('index.html', bootstrap={
+        'headers': list(raw[0].keys()),
+        'data': [list(item.values()) for item in raw]
+    })
 
 
 @app.route('/api/dataset/<int:experiment_id>')
