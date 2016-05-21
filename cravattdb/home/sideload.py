fc998e83a99c3644c0e6ba87cd103b4ee2300ba0
Radu Suciu <radusuciu@gmail.com>
2016-05-20 19:22:52 -0700

    Working zip extraction and dataset insertion

--- cravattdb/home/sideload.py
+++ cravattdb/home/sideload.py
@@ -1,72 +1,66 @@
 """Helper methods for creating datasets."""
-from cravattdb import db
+from cravattdb import app, db
 from cravattdb.home.models import Dataset
 from zipfile import ZipFile
-# from cravattdb import app
 import csv
-import io
 import pathlib
 
 
 def new_dataset(dataset_id, user_id, file):
+
+    cimage_data_path = unzip_cimage(file, user_id, dataset_id)
+    output_file_path = pathlib.Path(
+        cimage_data_path,
+        'dta',
+        'output',
+        'output_rt_10_sn_2.5.to_excel.txt'
+    )
+
     dataset = create_dataset(dataset_id)
     db.create_all()
 
-    cimage_output_file = unzip_cimage(file, user_id, dataset_id)
-    return
+    with output_file_path.open('r') as f:
+        # skip first line
+        f.readline()
 
-    raw_data = preprocess(file)
-    tsv = io.StringIO(raw_data)
+        for line in csv.reader(f, delimiter='\t'):
+            db.session.add(dataset(
+                peptide_index=line[0],
+                ipi=line[1],
+                description=line[2],
+                symbol=line[3],
+                sequence=line[4],
+                mass=line[5],
+                charge=line[6],
+                segment=line[7],
+                ratio=line[8],
+                intensity=line[9],
+                num_ms2_peaks=line[10].split('/')[0],
+                num_candidate_peaks=line[10].split('/')[1],
+                max_light_intensity=line[10].split('/')[2],
+                light_noise=line[10].split('/')[3],
+                max_heavy_intensity=line[10].split('/')[4],
+                heavy_noise=line[10].split('/')[5],
+                rsquared=line[11],
+                entry=line[12],
+                link=line[13]
+            ))
 
-    for line in csv.reader(tsv, delimiter='\t'):
-        db.session.add(dataset(
-            peptide_index=line[0],
-            ipi=line[1],
-            description=line[2],
-            symbol=line[3],
-            sequence=line[4],
-            mass=line[5],
-            charge=line[6],
-            segment=line[7],
-            ratio=line[8],
-            intensity=line[9],
-            num_ms2_peaks=line[10].split('/')[0],
-            num_candidate_peaks=line[10].split('/')[1],
-            max_light_intensity=line[10].split('/')[2],
-            light_noise=line[10].split('/')[3],
-            max_heavy_intensity=line[10].split('/')[4],
-            heavy_noise=line[10].split('/')[5],
-            rsquared=line[11],
-            entry=line[12],
-            link=line[13]
-        ))
-
-    # db.session.commit()
+    db.session.commit()
 
 
 def unzip_cimage(file, user_id, dataset_id):
     extract_path = pathlib.Path(
-        # app.instance_path,
+        app.instance_path,
         'legacy',
         str(user_id),
         str(dataset_id)
     )
 
-    print(str(extract_path))
-
     with ZipFile(file, 'r') as cimage_zip:
         cimage_zip.extractall(str(extract_path))
 
-    return
-
-
-def preprocess(file):
-    # skip first line
-    next(file.stream)
-    raw_data = file.read()
-    file.close()
-
-    return raw_data.decode()
+    return extract_path
 
 
 def create_dataset(dataset_id):
