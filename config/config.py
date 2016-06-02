58f965042381f38a52d8ec120a8c8d8f945c6ed6
Radu Suciu <radusuciu@gmail.com>
2016-06-01 18:25:30 -0700

    Fixes to secrets

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
-"""Main configuration file for project."""
+"""ONLY (kind of :/) configuration file for project."""
 import yaml
 import os
 import pathlib
 
+
 # debug is true by default
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = bool(os.getenv('DEBUG', True))
 _secrets_path = 'config/secrets{}.yml'.format('' if DEBUG else '.prod')
 _override_path = 'config/secrets.override.yml'
 
@@ -17,6 +18,9 @@ if os.path.isfile(_override_path):
     with open(_override_path, 'r') as f:
         _SECRETS.update(yaml.load(f))
 
+# remove extraneous 'environment' wrapper without which docker-compose will bitch
+# this is only necessary to avoid duplication of secrets
+_SECRETS = {k: v['environment'] for k, v in _SECRETS.items()}
 
 PROJECT_NAME = 'cravattdb'
 PROJECT_HOME_PATH = pathlib.PurePath(os.path.realpath(__file__)).parents[1]
@@ -29,25 +33,27 @@ CONVERT_URL = 'http://cravattwork.scripps.edu:5001'
 class _Config(object):
     """Holds flask configuration to be consumed by Flask's from_object method."""
 
-    DEBUG = True
+    # Flask
+    DEBUG = False
     SECRET_KEY = _SECRETS['flask']['SECRET_KEY']
 
     # Flask-Security
     SECURITY_PASSWORD_SALT = _SECRETS['flask-security']['SECURITY_PASSWORD_SALT']
     SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
     SECURITY_REGISTERABLE = True
-
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
+    MAIL_DEFAULT_SENDER = ''
     EMAIL = _SECRETS['mail']['EMAIL']
     EMAIL_PASSWORD = _SECRETS['mail']['EMAIL_PASSWORD']
 
+    # Flask-SqlAlchemy
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@postgres/{}'.format(
-        _SECRETS['database']['environment']['DB_USER'],
-        _SECRETS['database']['environment']['DB_PASS'],
-        _SECRETS['database']['environment']['DB_NAME']
+        _SECRETS['database']['DB_USER'],
+        _SECRETS['database']['DB_PASS'],
+        _SECRETS['database']['DB_NAME']
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -59,4 +65,4 @@ class _DevelopmentConfig(_Config):
     DEBUG = True
 
 
-config = _Config if DEBUG else _DevelopmentConfig
+config = _DevelopmentConfig if DEBUG else _Config
