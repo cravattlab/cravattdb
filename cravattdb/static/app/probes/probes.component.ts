74ace3e038cc138aa0da8c569d7856af29b60332
Radu Suciu <radusuciu@gmail.com>
2016-07-29 13:38:10 -0700

    Basic display of probes. #10

--- cravattdb/static/app/probes/probes.component.ts
+++ cravattdb/static/app/probes/probes.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ProbesService } from './probes.service'
-
 
 @Component({
     templateUrl: 'static/app/probes/probes.html',
     providers: [ProbesService]
 })
 
-export class ProbesComponent {
+export class ProbesComponent implements OnInit {
     data: any[];
 
     constructor(private service: ProbesService) {}
 
     ngOnInit(): void {
         this.service.getData().then(d => this.data = d);
+    }
+
+    getStructure(inchi): string {
+        return `https://cactus.nci.nih.gov/chemical/structure/${inchi}/image?format=png&width=500&height=500&csymbol=none&hsymbol=special&border`;
     }
 }
