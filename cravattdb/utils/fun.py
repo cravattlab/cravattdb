bb8f62b548e78538400708cdc08ecbed04be67c1
Radu Suciu <radusuciu@gmail.com>
2016-08-15 11:48:20 -0700

    Add special median function. #15

---
+++ cravattdb/utils/fun.py
@@ -0,0 +1,11 @@
+"""Contains a few useful functions."""
+
+import statistics
+
+
+def special_median(ratios):
+    """Calculate median given a set of ratios. Modified with some empirical rules."""
+    if len(ratios) == 2 and 20 in ratios and min(ratios) < 3:
+        return min(ratios)
+
+    return statistics.median(ratios)
