d85a8b0f559a71afd7ac8480f01ff5cc2648e1fb
Radu Suciu <radusuciu@gmail.com>
2016-08-23 15:09:57 -0700

    Move to reactive forms. Closes #35 (#65)

--- cravattdb/static/app/auto/auto.component.ts
+++ cravattdb/static/app/auto/auto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
-import { AutoService } from './auto.service'
+import { AutoService } from './auto.service';
 
 import * as _ from 'lodash';
 
@@ -11,13 +12,18 @@ import * as _ from 'lodash';
 })
 
 export class AutoComponent implements OnInit {
+    autoForm: FormGroup;
     data: {} = {};
     showErrors: boolean = false;
     errors: any[] = [];
     files: any[] = [];
     progress: number = 0;
 
-    constructor(private service: AutoService, private zone: NgZone) {
+    constructor(
+        private service: AutoService,
+        private formBuilder : FormBuilder,
+        private zone: NgZone
+    ) {
         this.service.progress$.debounceTime(100).subscribe(progress => {
             // struggle bus report:
             // http://stackoverflow.com/a/37695136/383744
@@ -29,6 +35,29 @@ export class AutoComponent implements OnInit {
 
     ngOnInit(): void {
         this.service.getData().subscribe(d => this.data = d);
+
+        this.autoForm = this.formBuilder.group({
+            name: '',
+            organism: '',
+            type: '',
+            instrument: '',
+            treatment_type: '',
+            proteomic_fraction: '',
+            probe: this.formBuilder.group({
+                id: 0,
+                concentration: '',
+                time: 0,
+            }),
+            inhibitor: this.formBuilder.group({
+                id: 0,
+                concentration: '',
+                time: 0,
+            }),
+            sample_type: '',
+            cell_type: '',
+            ip2_username: '',
+            ip2_password: ''
+        });
     }
 
     onFileChange(e): void {
