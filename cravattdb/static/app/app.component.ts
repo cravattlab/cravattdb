fdba56d5f89503f7a2836c267431c4823d3631c2
Radu Suciu <radusuciu@gmail.com>
2016-08-28 16:05:36 -0700

    Move rxjs operator declaration to vendor bundle

--- cravattdb/static/app/app.component.ts
+++ cravattdb/static/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import './add-rxjs-operators';
 
 @Component({
     selector: 'app',
--- cravattdb/static/config/vendor.ts
+++ cravattdb/static/config/vendor.ts
@@ -8,7 +8,16 @@ import '@angular/router';
 import '@angular/forms';
 // RxJS
 import 'rxjs';
-// import './app/add-rxjs-operators';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/observable/forkJoin';
+
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'lodash';
