a7d23e0cc91c4b62fb5e99ee5f6503524b84d234
Radu Suciu <radusuciu@gmail.com>
2016-08-31 13:04:35 -0700

    Working dev bundling with webpack

--- cravattdb/static/app/main.ts
+++ cravattdb/static/app/main.ts
@@ -2,6 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/share';
+
+
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
--- cravattdb/static/config/webpack.dev.js
+++ cravattdb/static/config/webpack.dev.js
@@ -1,24 +1,82 @@
-var webpackMerge = require('webpack-merge');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
-module.exports = webpackMerge(commonConfig, {
-  // devtool: 'cheap-module-eval-source-map',
+module.exports = {
+  entry: [
+    './config/polyfills.ts',
+    './config/vendor.ts',
+    './app/main.ts'
+  ],
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/static/dist',
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
+    publicPath: '/static/dist/',
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    // resolve module file requests by looking for explicit extensions
+    // or look for matching files with .js or .ts extensions
+    extensions: ['', '.js', '.ts']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw'
+      },
+      // handle component-scoped styles specified with styleUrls
+      {
+        test: /\.css$/,
+        include: helpers.root('app'),
+        loader: 'raw'
+      }
+    ]
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: require(helpers.root('dist', 'vendor-manifest.json'))
+    }),
+    new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: require(helpers.root('dist', 'polyfills-manifest.json'))
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'app/index.html',
+      filename: '../../templates/index.html'
+    }),
+    new AddAssetHtmlPlugin([
+      { filepath: 'dist/polyfills.dll.js', includeSourcemap: false },
+      { filepath: 'dist/vendor.dll.js', includeSourcemap: false }
+    ]),
+    new webpack.ProvidePlugin({
+      chroma: 'chroma-js',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      $: 'jquery'
+    })
   ],
 
   devServer: {
-    historyApiFallback: true,
-    stats: 'minimal'
+    host: '0.0.0.0',
+    port: 3000,
+    profile: true,
+    progress: true
+  },
+  // needed for dev work in a network share from a VM
+  watchOptions: {
+    poll: true
   }
-});
+};
